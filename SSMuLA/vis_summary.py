a6197c857c7947e6a9275ee70f81fe6a226c513f
francescazfl <francesca.zf.l@berkeley.edu>
2024-05-23 23:31:10 -0700

    add new ds zs

--- SSMuLA/vis_summary.py
+++ SSMuLA/vis_summary.py
@@ -415,7 +415,7 @@ class ZSSSumVis(SumVis):
 
 def plot_de_v_mlde(
     plot_folder: str = "results/de_vs_mlde/onehot/collage/n_samples",
-    mlde_csv: str = "results/mlde/vis_3/all_df.csv",
+    mlde_csv: str = "results/mlde/vis_5/all_df.csv",
     de_folder: str = "results/de/DE-active/scale2max",
     n_mut: str = "all",
     n_top: int = 96,
@@ -549,7 +549,7 @@ def plot_de_v_mlde(
 
 def plot_n_ftmlde(
     plot_folder: str = "results/de_vs_mlde/onehot/collage/ftMLDE",
-    mlde_csv: str = "results/mlde/vis_3/all_df.csv",
+    mlde_csv: str = "results/mlde/vis_5/all_df.csv",
     de_folder: str = "results/simulations/DE-active/scale2max",
     simplezs: bool = True,
     min_ftlib: bool = True,
