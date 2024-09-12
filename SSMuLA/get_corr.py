189f23a203ab6acb02340cfc6b0e8c54e959c0be
francescazfl <francesca.zf.l@berkeley.edu>
2024-09-11 18:57:47 -0700

    add ftalde alde fig2b

--- SSMuLA/get_corr.py
+++ SSMuLA/get_corr.py
@@ -180,13 +180,10 @@ DT_METRIC = {
 FTLIB_FRAC_LIST = [0.125, 0.25, 0.5, 1]
 
 
-class CorrPerfomanceCharacter:
+class MergeLandscapeAttributes:
     """
-    A class for getting the correlation between the performance and the characteristics of the library
-
-    TODO:
-    Add model opts
-    Add DE opts
+    A class for merging the landscape attributes
+    aka all non (ft)MLDE and (ft)ALDE results
     """
 
     def __init__(
@@ -194,17 +191,10 @@ class CorrPerfomanceCharacter:
         lib_stat_path: str = "results/fitness_distribution/max/all_lib_stats.csv",
         loc_opt_path: str = "results/local_optima/scale2max.csv",
         pwe_path: str = "results/pairwise_epistasis_vis/none/scale2max.csv",
-        zs_path: str = "results/zs_sum/none/zs_stat_scale2max.csv",
+        zs_path: str = "results/zs_sum_5/none/zs_stat_scale2max.csv",
         de_path: str = "results/de/DE-active/scale2max/all_landscape_de_summary.csv",
-        mlde_path: str = "results/mlde/all_df_comb_onehot_2.csv",
-        corr_dir: str = "results/corr",
-        n_mut_cuttoff: int = 0,
-        n_sample: int = 384,
-        n_top: int = 96,
-        filter_active: float = 1,
-        ft_frac=0.125,
-        models: list[str] = ["boosting", "ridge"],
-        ifplot: bool = True,
+        merge_dir: str = "results/merged",
+        n_mut_cutoff: int = 0,
     ):
 
         self._lib_stat_path = lib_stat_path
@@ -212,31 +202,13 @@ class CorrPerfomanceCharacter:
         self._pwe_path = pwe_path
         self._zs_path = zs_path
         self._de_path = de_path
-        self._mlde_path = mlde_path
-        self._corr_dir = checkNgen_folder(corr_dir)
-
-        self._n_mut_cuttoff = n_mut_cuttoff
-        self._n_mut = n_mut_cutoff_dict[self._n_mut_cuttoff]
-        self._n_sample = n_sample
-        self._n_top = n_top
-        self._filter_active = filter_active
-        self._ft_frac = ft_frac
-        self._models = models
-
-        join_model = "|".join(models)
-        self._corr_subdir = checkNgen_folder(
-            os.path.join(
-                "_".join([self._corr_dir, self._n_mut]),
-                str(n_sample),
-                f"{join_model}-top{str(n_top)}",
-                f"actcut-{str(filter_active)}",
-            )
-        )
-
-        print(f"Correlation directory: {self._corr_subdir}")
+        self._merge_dir = checkNgen_folder(merge_dir)
+        
+        self._n_mut_cutoff = n_mut_cutoff
+        self._n_mut = n_mut_cutoff_dict[self._n_mut_cutoff]
 
         self._lib_stat_df = (
-            parse_lib_stat(self._lib_stat_path, self._n_mut_cuttoff)
+            parse_lib_stat(self._lib_stat_path, self._n_mut_cutoff)
             .reset_index(drop=True)
             .copy()
         )
@@ -259,39 +231,13 @@ class CorrPerfomanceCharacter:
         print(f"Loaded de from: {self._de_path} for libs:")
         print(self._de_stat_df["lib"].unique())
 
-        self._mlde_stat_df, self._zs_list = self._get_mlde_stat()
-        print(f"Loaded mlde from: {self._mlde_path} for libs:")
-        print(self._mlde_stat_df["lib"].unique())
-
         # now merge all
-        self._merge_all_df = self._get_merge_all()
+        self._merge_all_attribute_df = self._merge_all_attributes()
 
-        # remove super dead libs 
-        self._active_lib_list = self._lib_stat_df[
-            (
-                self._lib_stat_df["percent_active"] >= self._filter_active
-            )  # & (self._lib_stat_df["percent_measured"] >= 90)
-        ]["lib"].tolist()
-
-        print(
-            f"For {self._filter_active} percent_active cutoff, active lib list: {self._active_lib_list}"
+        self._merge_all_attribute_df.to_csv(
+            f"{self._merge_dir}/merge_landscapeattributes_{self._n_mut}.csv", index=False
         )
 
-        self._actcutt_df = self._merge_all_df[
-            self._merge_all_df["lib"].isin(self._active_lib_list)
-        ]
-        self._actcutt_df.to_csv(
-            f"{self._corr_subdir}/merge_all_{str(self._ft_frac)}.csv", index=False
-        )
-
-        self._corr_df = self._get_corr_df()
-        self._corr_df.to_csv(
-            f"{self._corr_subdir}/corr_{str(self._ft_frac)}.csv", index=False
-        )
-
-        # now plot
-        if ifplot:
-            self._plot_corr()
 
     def _get_loc_opt(self) -> pd.DataFrame:
         """
@@ -378,6 +324,197 @@ class CorrPerfomanceCharacter:
             )
 
         return de_df_concat.copy(), de_types
+
+    def _get_pwe(self) -> pd.DataFrame:
+
+        """
+        A method to get the pairwise epistasis data
+        """
+
+        df_pw = pd.read_csv(self._pwe_path)
+
+        df_pw_s_rs = (
+            df_pw[
+                (df_pw["n_mut"] == "all")
+                & (df_pw["summary_type"] == "fraction")
+                & (df_pw["epistasis_type"] != "magnitude")
+            ][["lib", "value"]]
+            .groupby("lib")
+            .sum()
+            .rename(columns={"value": "fraction_non-magnitude"})
+        )
+
+        df_pw_rs = df_pw[
+            (df_pw["n_mut"] == "all")
+            & (df_pw["summary_type"] == "fraction")
+            & (df_pw["epistasis_type"] == "reciprocal sign")
+        ][["lib", "value"]].rename(columns={"value": "fraction_reciprocal-sign"})
+
+        return pd.merge(df_pw_s_rs, df_pw_rs, on="lib").copy()
+
+    def _merge_all_attributes(self) -> pd.DataFrame:
+
+        """
+        Merge all the dataframes
+        """
+
+        merge_df = pd.merge(
+            # pd.merge(
+                pd.merge(
+                    pd.merge(
+                        pd.merge(self._lib_stat_df, self._loc_opt_df, on="lib"),
+                        self._pwe_df,
+                        on="lib",
+                    ),
+                    self._zs_stat_df,
+                    on="lib",
+                ),
+                self._de_stat_df,
+                on="lib",
+            # ),
+            # self._mlde_stat_df,
+            # on="lib",
+        )
+
+        # numb_loc_opt
+        # frac_loc_opt_total
+        # frac_loc_opt_hd2_escape_numb
+        # frac_loc_opt_hd2_cannot_escape_numb
+        # numb_loc_opt_norm_cannot_escape
+        # frac_loc_opt_norm_cannot_escape
+        merge_df["numb_loc_opt_norm_cannot_escape"] = (
+            merge_df["numb_loc_opt"] * merge_df["frac_loc_opt_hd2_cannot_escape_numb"]
+        )
+        merge_df["frac_loc_opt_norm_cannot_escape"] = (
+            merge_df["frac_loc_opt_total"]
+            * merge_df["frac_loc_opt_hd2_cannot_escape_numb"]
+        )
+
+        merge_df["norm_non-magnitude"] = (
+            merge_df["fraction_non-magnitude"] * merge_df["percent_active"]
+        )
+        merge_df["norm_reciprocal-sign"] = (
+            merge_df["fraction_reciprocal-sign"] * merge_df["percent_active"]
+        )
+
+        return merge_df.copy()
+
+    @property
+    def lib_stat_df(self) -> pd.DataFrame:
+        """Return the library statistics dataframe"""
+        return self._lib_stat_df
+
+    @property
+    def all_libs(self) -> list:
+        """Return the list of all libraries"""
+        return sorted(self._lib_stat_df["lib"].unique())
+
+    @property
+    def loc_opt_df(self) -> pd.DataFrame:
+        """Return the local optima dataframe"""
+        return self._loc_opt_df
+
+    @property
+    def zs_stat_df(self) -> pd.DataFrame:
+        """Return the zs stat dataframe"""
+        return self._zs_stat_df
+
+    @property
+    def de_stat_df(self) -> pd.DataFrame:
+        """Return the DE statistics dataframe"""
+        return self._de_stat_df
+
+    @property
+    def merge_all_attribute_df(self) -> pd.DataFrame:
+        """Return the merged dataframe"""
+        return self._merge_all_attribute_df
+
+
+class CorrPerfomanceCharacter(MergeLandscapeAttributes):
+    """
+    A class for getting the correlation between the performance and the characteristics of the library
+
+    TODO:
+    Add model opts
+    Add DE opts
+    """
+
+    def __init__(
+        self,
+        lib_stat_path: str = "results/fitness_distribution/max/all_lib_stats.csv",
+        loc_opt_path: str = "results/local_optima/scale2max.csv",
+        pwe_path: str = "results/pairwise_epistasis_vis/none/scale2max.csv",
+        zs_path: str = "results/zs_sum/none/zs_stat_scale2max.csv",
+        de_path: str = "results/de/DE-active/scale2max/all_landscape_de_summary.csv",
+        mlde_path: str = "results/mlde/all_df_comb_onehot_2.csv",
+        merge_dir: str = "results/merged",
+        corr_dir: str = "results/corr",
+        n_mut_cutoff: int = 0,
+        n_sample: int = 384,
+        n_top: int = 96,
+        filter_active: float = 1,
+        ft_frac=0.125,
+        models: list[str] = ["boosting", "ridge"],
+        ifplot: bool = True,
+    ):
+
+        super().__init__(lib_stat_path, loc_opt_path, pwe_path, zs_path, de_path, merge_dir, n_mut_cutoff)
+
+        self._mlde_path = mlde_path
+        self._corr_dir = checkNgen_folder(corr_dir)
+
+        self._n_sample = n_sample
+        self._n_top = n_top
+        self._filter_active = filter_active
+        self._ft_frac = ft_frac
+        self._models = models
+
+        join_model = "|".join(models)
+        self._corr_subdir = checkNgen_folder(
+            os.path.join(
+                "_".join([self._corr_dir, self._n_mut]),
+                str(n_sample),
+                f"{join_model}-top{str(n_top)}",
+                f"actcut-{str(filter_active)}",
+            )
+        )
+
+        print(f"Correlation directory: {self._corr_subdir}")
+
+        self._mlde_stat_df, self._zs_list = self._get_mlde_stat()
+        print(f"Loaded mlde from: {self._mlde_path} for libs:")
+        print(self._mlde_stat_df["lib"].unique())
+
+        # now merge all
+        self._merge_all_df = self._get_merge_all()
+
+        # remove super dead libs 
+        self._active_lib_list = self._lib_stat_df[
+            (
+                self._lib_stat_df["percent_active"] >= self._filter_active
+            )  # & (self._lib_stat_df["percent_measured"] >= 90)
+        ]["lib"].tolist()
+
+        print(
+            f"For {self._filter_active} percent_active cutoff, active lib list: {self._active_lib_list}"
+        )
+
+        self._actcutt_df = self._merge_all_df[
+            self._merge_all_df["lib"].isin(self._active_lib_list)
+        ]
+        self._actcutt_df.to_csv(
+            f"{self._corr_subdir}/merge_all_{str(self._ft_frac)}.csv", index=False
+        )
+
+        self._corr_df = self._get_corr_df()
+        self._corr_df.to_csv(
+            f"{self._corr_subdir}/corr_{str(self._ft_frac)}.csv", index=False
+        )
+
+        # now plot
+        if ifplot:
+            self._plot_corr()
+
 
     def _get_mlde_stat(self) -> pd.DataFrame:
 
@@ -502,32 +639,6 @@ class CorrPerfomanceCharacter:
 
         return mlde_avg.reset_index().copy(), zs_list
 
-    def _get_pwe(self) -> pd.DataFrame:
-
-        """
-        A method to get the pairwise epistasis data
-        """
-
-        df_pw = pd.read_csv(self._pwe_path)
-
-        df_pw_s_rs = (
-            df_pw[
-                (df_pw["n_mut"] == "all")
-                & (df_pw["summary_type"] == "fraction")
-                & (df_pw["epistasis_type"] != "magnitude")
-            ][["lib", "value"]]
-            .groupby("lib")
-            .sum()
-            .rename(columns={"value": "fraction_non-magnitude"})
-        )
-
-        df_pw_rs = df_pw[
-            (df_pw["n_mut"] == "all")
-            & (df_pw["summary_type"] == "fraction")
-            & (df_pw["epistasis_type"] == "reciprocal sign")
-        ][["lib", "value"]].rename(columns={"value": "fraction_reciprocal-sign"})
-
-        return pd.merge(df_pw_s_rs, df_pw_rs, on="lib").copy()
 
     def _get_merge_all(self) -> pd.DataFrame:
         """
@@ -535,19 +646,7 @@ class CorrPerfomanceCharacter:
         """
 
         merge_df = pd.merge(
-            pd.merge(
-                pd.merge(
-                    pd.merge(
-                        pd.merge(self._lib_stat_df, self._loc_opt_df, on="lib"),
-                        self._pwe_df,
-                        on="lib",
-                    ),
-                    self._zs_stat_df,
-                    on="lib",
-                ),
-                self._de_stat_df,
-                on="lib",
-            ),
+            self.merge_all_attribute_df,
             self._mlde_stat_df,
             on="lib",
         )
@@ -790,31 +889,6 @@ class CorrPerfomanceCharacter:
                     )
 
     @property
-    def lib_stat_df(self) -> pd.DataFrame:
-        """Return the library statistics dataframe"""
-        return self._lib_stat_df
-
-    @property
-    def all_libs(self) -> list:
-        """Return the list of all libraries"""
-        return sorted(self._lib_stat_df["lib"].unique())
-
-    @property
-    def loc_opt_df(self) -> pd.DataFrame:
-        """Return the local optima dataframe"""
-        return self._loc_opt_df
-
-    @property
-    def zs_stat_df(self) -> pd.DataFrame:
-        """Return the zs stat dataframe"""
-        return self._zs_stat_df
-
-    @property
-    def de_stat_df(self) -> pd.DataFrame:
-        """Return the DE statistics dataframe"""
-        return self._de_stat_df
-
-    @property
     def mlde_stat_df(self) -> pd.DataFrame:
         """Return the MLDE statistics dataframe"""
         return self._mlde_stat_df
@@ -847,8 +921,9 @@ def perfom_corr(
     zs_path: str = "results/zs_sum/none/zs_stat_scale2max.csv",
     de_path: str = "results/de/DE-active/scale2max/all_landscape_de_summary.csv",
     mlde_path: str = "results/mlde/all_df_comb_onehot_2.csv",
+    merge_dir: str = "results/merged",
     corr_dir: str = "results/corr",
-    n_mut_cuttoff: int = 0,
+    n_mut_cutoff: int = 0,
     filter_active: float = 1,
     ft_frac: float = 0.125,
     n_top_list: list[int] = [96, 384],
@@ -868,7 +943,7 @@ def perfom_corr(
                     de_path=de_path,
                     mlde_path=mlde_path,
                     corr_dir=corr_dir,
-                    n_mut_cuttoff=n_mut_cuttoff,
+                    n_mut_cutoff=n_mut_cutoff,
                     n_sample=n,
                     n_top=n_top,
                     filter_active=filter_active,
--- tests/test_corr.py
+++ tests/test_corr.py
@@ -5,7 +5,7 @@ import os
 
 from datetime import datetime
 
-from SSMuLA.get_corr import perfom_corr
+from SSMuLA.get_corr import MergeLandscapeAttributes, perfom_corr
 from SSMuLA.util import checkNgen_folder
 
 
@@ -20,8 +20,17 @@ if __name__ == "__main__":
     )
     sys.stdout = f
 
+    # MergeLandscapeAttributes(
+    #     lib_stat_path="results/fitness_distribution/max/all_lib_stats.csv",
+    #     loc_opt_path="results/local_optima/scale2max.csv",
+    #     pwe_path="results/pairwise_epistasis_vis/none/scale2max.csv",
+    #     zs_path="results/zs_sum_5/none/zs_stat_scale2max.csv",
+    #     de_path="results/de/DE-active/scale2max/all_landscape_de_summary.csv",
+    #     merge_dir="results/merged",
+    # )
+
     perfom_corr(
-        n_mut_cuttoff=0,
+        n_mut_cutoff=0,
         n_list=[384],
         zs_path="results/zs_sum_5/none/zs_stat_scale2max.csv",
         mlde_path="results/mlde/all_df_comb_onehot_2.csv",
@@ -30,7 +39,7 @@ if __name__ == "__main__":
     )
 
     perfom_corr(
-        n_mut_cuttoff=0,
+        n_mut_cutoff=0,
         n_list=[384],
         filter_active=0,
         zs_path="results/zs_sum_5/none/zs_stat_scale2max.csv",
@@ -40,7 +49,7 @@ if __name__ == "__main__":
     )
 
     # perfom_corr(
-    #     n_mut_cuttoff=2,
+    #     n_mut_cutoff=2,
     #     n_list=[384],
     #     zs_path="results/zs_sum_5/none/zs_stat_scale2max.csv",
     #     mlde_path="results/mlde/all_df_comb_onehot_2.csv",
@@ -49,6 +58,19 @@ if __name__ == "__main__":
     # )
 
     """
+
+    MergeLandscapeAttributes:
+
+    def __init__(
+        self,
+        lib_stat_path: str = "results/fitness_distribution/max/all_lib_stats.csv",
+        loc_opt_path: str = "results/local_optima/scale2max.csv",
+        pwe_path: str = "results/pairwise_epistasis_vis/none/scale2max.csv",
+        zs_path: str = "results/zs_sum_5/none/zs_stat_scale2max.csv",
+        de_path: str = "results/de/DE-active/scale2max/all_landscape_de_summary.csv",
+        merge_dir: str = "results/merged",
+    ):
+
     perfom_corr(
         lib_stat_path: str = "results/fitness_distribution/max/all_lib_stats.csv",
         loc_opt_path: str = "results/local_optima/scale2max.csv",
@@ -57,7 +79,7 @@ if __name__ == "__main__":
         de_path: str = "results/de/DE-active/scale2max/all_landscape_de_summary.csv",
         mlde_path: str = "results/mlde/all_df_comb_onehot_2.csv",
         corr_dir: str = "results/corr",
-        n_mut_cuttoff: int = 0,
+        n_mut_cutoff: int = 0,
         filter_active: float = 1,
         ft_frac: float = 0.125,
         n_top_list: list[int] = [96, 384],
