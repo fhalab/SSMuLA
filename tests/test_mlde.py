539431be1dfc81553b436d0fa340a0b74bc6df87
francescazfl <francesca.zf.l@berkeley.edu>
2024-10-12 17:05:47 -0700

    add more emb comp

--- tests/test_mlde.py
+++ tests/test_mlde.py
@@ -26,19 +26,79 @@ if __name__ == "__main__":
     # mlde_folder = "results/mlde_coves_2"
 
 
+    # run_all_mlde_parallelized(
+    #     encodings=DEFAULT_LEARNED_EMB_COMBO,
+    #     n_mut_cutoffs=[0],
+    #     zs_predictors=["none"],
+    #     n_samples=[384],
+    #     n_replicate=50,
+    #     n_job=96,
+    #     all_libs=False,
+    #     ft_first=True,
+    #     lib_list=["ParD2", "ParD3"],
+    #     mlde_folder="results/mlde_emb",
+    #     zs_folder=zs_folder,
+    # )
+
+
+
     run_all_mlde_parallelized(
         encodings=DEFAULT_LEARNED_EMB_COMBO,
         n_mut_cutoffs=[0],
-        zs_predictors=["none"],
+        zs_predictors=["ev"],
+        n_samples=[96, 384, 24, 48],
+        n_replicate=50,
+        n_job=96,
+        all_libs=False,
+        ft_first=True,
+        lib_list=["T7", "TEV"],
+        mlde_folder="results/mlde_emb",
+        zs_folder=zs_folder,
+    )
+
+    run_all_mlde_parallelized(
+        encodings=DEFAULT_LEARNED_EMB_COMBO,
+        n_mut_cutoffs=[0],
+        zs_predictors=["ev"],
+        n_samples=[192, 288, 480, 576],
+        n_replicate=50,
+        n_job=96,
+        all_libs=False,
+        ft_first=True,
+        lib_list=["T7", "TEV"],
+        mlde_folder="results/mlde_emb",
+        zs_folder=zs_folder,
+    )
+
+    run_all_mlde_parallelized(
+        encodings=DEFAULT_LEARNED_EMB_COMBO,
+        n_mut_cutoffs=[0],
+        zs_predictors=["ev"],
+        n_samples=[960, 1920],
+        n_replicate=50,
+        n_job=96,
+        all_libs=False,
+        ft_first=True,
+        lib_list=["T7", "TEV"],
+        mlde_folder="results/mlde_emb",
+        zs_folder=zs_folder,
+    )
+
+
+    run_all_mlde_parallelized(
+        encodings=["one-hot"],
+        n_mut_cutoffs=[0],
+        zs_predictors=["ev"],
         n_samples=[384],
         n_replicate=50,
         n_job=96,
         all_libs=False,
         ft_first=True,
-        lib_list=["ParD2", "ParD3"],
+        lib_list=["DHFR", "GB1", "ParD2", "ParD3", "TrpB3A", "TrpB3B", "TrpB3C", "TrpB3D", "TrpB3E", "TrpB3F", "TrpB3G", "TrpB3H", "TrpB3I", "TrpB4"],
         mlde_folder="results/mlde_emb",
         zs_folder=zs_folder,
     )
+
 
 
     # run_all_mlde_parallelized(
